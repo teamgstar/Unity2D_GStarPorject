e09d25948d5e84b22041a446939587c756b9cafc
mjun1208 <37032675+mjun1208@users.noreply.github.com>
2019-05-29 11:35:20 +0900

    라인 추가

---
+++ Assets/mj/Scripts/PlayerLine.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLine : MonoBehaviour
+{
+    public GameObject line;
+    public GameObject Player;
+    private SpriteRenderer render;
+    void Start()
+    {
+        
+        render = line.GetComponent<SpriteRenderer>();
+        //playerScript.Rot;
+        render.enabled = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Debug.Log(PlayerMovement.Rot);
+
+        if(PlayerMovement.m_Status == PlayerMovement.PlayerStatus.Slide_On)
+        {
+            this.transform.position = Player.transform.position;
+            render.enabled = true;
+        }
+        if(PlayerMovement.m_Status == PlayerMovement.PlayerStatus.Slide_Out)
+        {
+            render.enabled = false;
+        }
+        
+        //Rot = playerScript.Rot;S
+        //this.transform.Rotate(new Vector3(0 ,0, PlayerMovement.Rot));
+        this.transform.rotation =  Quaternion.Euler(0, 0, PlayerMovement.Rot + 90);
+        this.transform.localScale = new Vector3(1,  Mathf.Abs(Vector2.Distance(PlayerMovement.m_vSlideEndPos , PlayerMovement.m_vSlideStartPos)) / 100 , 1);
+    }
+}
--- Assets/mj/Scripts/PlayerMovement.cs
+++ Assets/mj/Scripts/PlayerMovement.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     Camera Camera;
 
 
-    private enum PlayerStatus : int
+    public  enum PlayerStatus : int
     {
         Start,
         Idle,
@@ -16,30 +16,18 @@ public class Player : MonoBehaviour
         Slide_Move
     }
 
-    PlayerStatus m_Status;
+   static public PlayerStatus m_Status;
 
-    private Vector2 m_vSlideStartPos = Vector2.zero;
-    private Vector2 m_vSlideEndPos = Vector2.zero;
+    static public Vector2 m_vSlideStartPos = Vector2.zero;
+    static public Vector2 m_vSlideEndPos = Vector2.zero;
     private Vector2 m_vDir = Vector2.zero;
-    private LineRenderer lineRenderer;
 
+    static public float Rot = 0;
     // Start is called before the first frame update
     void Start()
     {
         m_Status = PlayerStatus.Start;
         Camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-
-
-        lineRenderer = GetComponent<LineRenderer>();
-        // lineRenderer.SetColors(Color.red, Color.yellow);
-        lineRenderer.startWidth = 1.0f;
-        lineRenderer.endWidth = 1.0f;
-        lineRenderer.startColor = Color.green;
-        lineRenderer.endColor = Color.green;
-        //라인렌더러 처음위치 나중위치
-
-
-
     }
 
     // Update is called once per frame
@@ -56,8 +44,11 @@ public class Player : MonoBehaviour
                     Debug.Log("Down");
                     m_Status = PlayerStatus.Slide_On;
                     m_vSlideStartPos = Input.mousePosition;
+
+
                //     m_vSlideStartPos = Camera.ScreenToWorldPoint(m_vSlideStartPos);
                 }
+    
 //#elif UNITY_ANDROID // 안드로이드
 //        if (Input.GetTouch(0).phase == TouchPhase.Began)
 //        {
@@ -66,6 +57,7 @@ public class Player : MonoBehaviour
 //#endif
                 break;
             case PlayerStatus.Slide_On:
+                
                 if (Input.GetKeyUp(KeyCode.Mouse0))
                 {
                     Debug.Log("Up");
@@ -76,14 +68,20 @@ public class Player : MonoBehaviour
                     m_vDir = m_vSlideEndPos - m_vSlideStartPos;
              
                     m_vDir.Normalize();
-                    float Rot = Mathf.Atan2(-m_vDir.y, -m_vDir.x) * 180 / Mathf.PI;
+                    Rot = Mathf.Atan2(-m_vDir.y, -m_vDir.x) * 180 / Mathf.PI;
                     //transform.Rotate(0.0f, 0.0f,Quaternion.FromToRotation(Vector3.up, m_vSlideEndPos - m_vSlideStartPos).z - 180);
                     //transform.rotation.z = 0;
                     Debug.Log(Rot);
     
                 }
-                lineRenderer.SetPosition(0, m_vSlideStartPos);
-                lineRenderer.SetPosition(1, Input.mousePosition);
+                else
+                {
+                    m_vSlideEndPos = Input.mousePosition;
+                    m_vDir = m_vSlideEndPos - m_vSlideStartPos;
+
+                    m_vDir.Normalize();
+                    Rot = Mathf.Atan2(-m_vDir.y, -m_vDir.x) * 180 / Mathf.PI;
+                }
                  
                 break;
             case PlayerStatus.Slide_Out:
