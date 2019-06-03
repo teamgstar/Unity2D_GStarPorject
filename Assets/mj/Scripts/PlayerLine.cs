2ef4797fdfc9db69b44dcb79becf43f92dfea4d2
MK <godekd3133@naver.com>
2019-06-03 20:17:50 +0900

    no message

--- Assets/mj/Scripts/PlayerLine.cs
+++ Assets/mj/Scripts/PlayerLine.cs
@@ -6,36 +6,71 @@ public class PlayerLine : MonoBehaviour
 {
     private const float m_MaxSize = 100f;
 
+    private Vector2 m_DownPos;
+    private Vector2 m_UpPos;
+
     public GameObject line;
+    public GameObject ClickLine;
+
     private GameObject m_Player;
     private SpriteRenderer m_Render;
 
     private PlayerMovement m_PlayerMovement;
+    private LineRenderer m_LineRenderer;
 
+    // Use this for initialization
+
+    private void Awake()
+    {
+
+        //라인렌더러 설정
+ 
+    }
     void Start()
     {
-      
+        m_LineRenderer = ClickLine.GetComponent<LineRenderer>();       
+        m_LineRenderer.startColor = Color.red;
+        m_LineRenderer.endColor = Color.yellow;
+        m_LineRenderer.startWidth = 0.1f;
+        m_LineRenderer.endWidth = 0.1f;
+        m_LineRenderer.enabled = false;
         m_Render = line.GetComponent<SpriteRenderer>();
-        m_Render.enabled = false;
-
         m_Player = GameObject.FindWithTag("Player");
         m_PlayerMovement = m_Player.GetComponent<PlayerMovement>();
+        m_LineRenderer.SetPosition(0, transform.position);
+        m_LineRenderer.SetPosition(1, transform.position);
+
+        m_Render.enabled = false;
+
+
+        m_DownPos = Vector2.zero;
+        m_UpPos = Vector2.zero;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-
-        if (m_PlayerMovement.m_Status == PlayerMovement.PlayerStatus.PS_Press)
+        switch(m_PlayerMovement.m_Status)
         {
-            Vector3 PlayerPos = m_Player.transform.position;
-            this.transform.position = PlayerPos;
-            m_Render.enabled = true;
+            case PlayerMovement.PlayerStatus.PS_Press:
+                this.transform.position = m_Player.transform.position;
+                m_LineRenderer.enabled = true;
+                m_Render.enabled = true;
+                break;
+
+            
         }
-        else
+
+        m_DownPos = m_PlayerMovement.m_DownPos;
+        m_UpPos = m_PlayerMovement.m_UpPos;
+        m_LineRenderer.SetPosition(0, m_DownPos);
+        m_LineRenderer.SetPosition(1, m_UpPos);
+
+        if (m_PlayerMovement.m_Status != PlayerMovement.PlayerStatus.PS_Press)
         {
             this.transform.localScale = Vector3.zero;
+            m_LineRenderer.enabled = false;
             m_Render.enabled = false;
         }
 
@@ -43,7 +78,7 @@ public class PlayerLine : MonoBehaviour
         //Rot = playerScript.Rot;S
         //this.transform.Rotate(new Vector3(0 ,0, PlayerMovement.Rot));
         this.transform.rotation =  Quaternion.Euler(0, 0, m_PlayerMovement.m_Rot + 90);
-        float distance = Mathf.Abs(Vector2.Distance(m_PlayerMovement.m_UpPos, m_Player.transform.position));
+        float distance = Mathf.Abs(Vector2.Distance(m_PlayerMovement.m_UpPos, m_PlayerMovement.m_DownPos));
 
         if (distance  * m_Render.sprite.texture.width > m_MaxSize)
         { distance = m_MaxSize / m_Render.sprite.texture.width; }
--- Assets/mj/Scripts/PlayerMovement.cs
+++ Assets/mj/Scripts/PlayerMovement.cs
@@ -20,13 +20,17 @@ public class PlayerMovement : MonoBehaviour
     [HideInInspector] public Vector2 m_DownPos;
     private Camera m_Camera;
 
+    private void Awake()
+    {
+        m_Rot = 0.0f;
+        m_Direction = Vector2.zero;
+        m_UpPos = Vector2.zero;
+        m_DownPos = Vector2.zero;
+        m_Status = PlayerStatus.PS_Idle;
+    }
+
     void Start()
     {
-        m_Rot       = 0.0f;
-        m_Direction = Vector2.zero;
-        m_UpPos     = Vector2.zero;
-        m_DownPos   = Vector2.zero;
-        m_Status    = PlayerStatus.PS_Idle;
         m_Camera    = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
@@ -39,36 +43,35 @@ public class PlayerMovement : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.Mouse0))
                 {
                     m_Status    = PlayerStatus.PS_Press;
-                    m_DownPos = Input.mousePosition;
-                    m_DownPos = Camera.main.ScreenToWorldPoint(m_DownPos);
+                    m_DownPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 }
                 break;
             case PlayerStatus.PS_Press:
-                GameManager.g_GameSpeed = 0.25f;
-
-                m_UpPos = Input.mousePosition;
-                m_UpPos = Camera.main.ScreenToWorldPoint(m_UpPos);
-                m_Direction = m_UpPos - (Vector2)this.transform.position;
+                GameManager.g_GameSpeed = 0.1f;
+                m_UpPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                m_Direction = m_UpPos - m_DownPos;
                 m_Direction.Normalize();
                 m_Rot = Mathf.Atan2(-m_Direction.y, -m_Direction.x) * 180 / Mathf.PI;
                 if (Input.GetKeyUp(KeyCode.Mouse0))
                 {
-                    m_UpPos = Input.mousePosition;
-                    m_UpPos = Camera.main.ScreenToWorldPoint(m_UpPos);
-                    m_Direction = m_UpPos - (Vector2)this.transform.position;
+                    m_UpPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                    m_Direction = m_UpPos - m_DownPos;
                     m_Direction.Normalize();
+
                     m_Rot = Mathf.Atan2(-m_Direction.y, -m_Direction.x) * 180.0f / Mathf.PI;
+               
                     this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                    this.GetComponent<Rigidbody2D>().AddForce(m_Direction * MoveSpeed, ForceMode2D.Impulse);
+                    this.GetComponent<Rigidbody2D>().AddForce(m_Direction * MoveSpeed * Time.deltaTime * 10, ForceMode2D.Impulse);
+
                     m_Status = PlayerStatus.PS_Move;
                 }
                 break;
             case PlayerStatus.PS_Move:
                 if(Input.GetKeyDown(KeyCode.Mouse0))
                 {
-                    m_DownPos = Input.mousePosition;
-                    m_DownPos = Camera.main.ScreenToWorldPoint(m_DownPos);
-                     m_Status = PlayerStatus.PS_Press;
+                    m_DownPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    m_Status = PlayerStatus.PS_Press;
                     this.GetComponent<Rigidbody2D>().velocity *= GameManager.g_GameSpeed;           
                 }
                 break;
